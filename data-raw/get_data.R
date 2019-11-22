7b3704875ca8e7e1bc1c3200e937c7ee427fb51e
BradXPS_Kraken <bradley.lindblad@gmail.com>
2019-11-22 16:13:44 -0600

    Getting ready for release

--- data-raw/get_data.R
+++ data-raw/get_data.R
@@ -13,19 +13,23 @@ final <- tibble(index = seq_along(stacked), text = stacked)
 theoffice <- final %>%
   tidyr::separate(text, c("ep", "txt"), sep = ";") %>%
   tidyr::drop_na() %>%
-  dplyr::mutate(actor = stringi::stri_extract_first_words(txt)) %>%
+  dplyr::mutate(character = stringi::stri_extract_first_words(txt)) %>%
   dplyr::rowwise() %>%
   dplyr::mutate(text = stringr::str_split(txt, ":")[[1]][2]) %>%
   dplyr::select(-txt) %>%
-  dplyr::mutate(episode = stringi::stri_sub(ep, 1, 2)) %>%
-  dplyr::mutate(season = stringi::stri_sub(ep, 4, 5)) %>%
+  dplyr::mutate(season = stringi::stri_sub(ep, 1, 2)) %>%
+  dplyr::mutate(episode = stringi::stri_sub(ep, 4, 5)) %>%
   dplyr::mutate(episode_name = stringr::str_split(ep, "-")[[1]][2]) %>%
   dplyr::select(-ep) %>%
   dplyr::rename(text_w_direction = text) %>%
   dplyr::mutate(text = sub("\\[.*] ", "", text_w_direction)) %>%
-  dplyr::select(index, season, episode, episode_name, actor, text, text_w_direction) %>%
-  tidyr::drop_na()
+  dplyr::select(index, season, episode, episode_name, character, text, text_w_direction) %>%
+  dplyr::filter(!stringi::stri_detect(str = episode_name, regex = "Webisode")) %>%
+  dplyr::filter(!stringi::stri_detect(str = episode_name, regex = "Deleted")) %>%
+  dplyr::filter(!stringi::stri_detect(str = episode_name, regex = "Gag")) %>%
+  tidyr::drop_na() %>%
+  dplyr::arrange(season) %>%
+  dplyr::arrange(episode)
 
-
-usethis::use_data(theoffice)
+usethis::use_data(theoffice, overwrite = TRUE)
 
